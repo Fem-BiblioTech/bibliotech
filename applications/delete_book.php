fd537323793214486bfe4d786327255d34c8b1da
rcarabal2022 <raquelcaraballospain@gmail.com>
2023-01-30 14:53:38 +0100

    Delete OK

---
+++ applications/delete_book.php
@@ -0,0 +1,15 @@
+<?php
+include("../db.php");
+ if (isset($_GET['isbn'])) {
+    $id = $_GET['isbn'];
+     $query = "DELETE FROM books WHERE isbn=".$id."";
+     $result = mysqli_query($conn, $query);
+     header ("Location: /bibliotech/");
+       }
+    else 
+        echo("Búsqueda fallida");
+
+    $_SESSION['message'] = "Libro eliminado con éxito";
+    $_SESSION['message_type'] = 'danger';
+
+?>
--- applications/footer.php
+++ applications/footer.php
@@ -8,7 +8,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-      <form action="applications/save_book.php" method="POST" enctype='multipart/form-data'>
+      <form action="/bibliotech/applications/save_book.php" method="POST" enctype='multipart/form-data'>
             
             <div class="form-group"><label for="title">Título</label>
             <input type="text" name="title" id="title" class= "form-control" placeholder="Agrega el título" autofocus></div>
